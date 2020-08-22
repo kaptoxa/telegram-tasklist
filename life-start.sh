5149e8feff19e09adbcd536a8bb4bdd7e6ccf438
Alex <alex.blednov@gmail.com>
2020-08-22 22:50:51 +0300

    Changes to be committed: 	deleted:    life-start.sh 	deleted:    life-stop.sh

--- life-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pgrep -f life.py && echo life already running && exit 1
-date >> life.log
-python3.8 life.py >> life.log 2>&1 &
-echo started new life!
--- life-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pid=`pgrep -f life.py | awk '{print $1}'`
-pgrep -f life.py && echo  running && kill -9 $pid && echo life was stopped
-
